2d1153b8fbef16ca068250bd691093519c5403fd
Eugene Keya <135608535+Keyaeugene@users.noreply.github.com>
2026-03-25 18:05:24 +0300

    Refactor image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: { 
     remotePatterns: [ 
-      { hostname: "next-moose-65.convex.cloud", protocol: "https"},
-      { hostname: "next-moose-65.convex.cloud", protocol: "https"},
+      { hostname: "next-moose-65.convex.cloud", protocol: "https" },
     ],
   },
 };
